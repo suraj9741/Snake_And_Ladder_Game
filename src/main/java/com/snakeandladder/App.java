4070d7e37c6df7cacfb6db25c99a806af70ff0d1
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 23:09:50 +0530

    Add function for check codition and perform operations

--- src/main/java/com/snakeandladder/App.java
+++ src/main/java/com/snakeandladder/App.java
@@ -11,11 +11,28 @@ public class App
 		int diceValue = (random.nextInt(10) % 6)+1;
 		return diceValue;
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
-        int startPosition = 0;
-        int player1Position = startPosition;
-        System.out.println(rollDie());
-    }
+	static int checkCondition(int player1Position) {
+		int condition = (random.nextInt(10) % 3);
+		switch(condition) {
+			case 0 :
+				player1Position = player1Position + 0;
+				break;
+			case 1 :
+				player1Position = player1Position + rollDie();
+				break;
+			case 2 :
+				player1Position = player1Position - rollDie();
+				break;
+			default :
+				break;
+		}
+		return player1Position;
+	}
+	public static void main( String[] args )
+	{
+		System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
+		int startPosition = 0;
+		int player1Position = startPosition;
+		System.out.println(checkCondition(player1Position));
+	}
 }
