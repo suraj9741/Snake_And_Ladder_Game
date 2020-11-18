ec3e88f460fdc534432b151948bdadbb740c0cf5
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 22:53:11 +0530

    Add function for Rolls the Die and get a number

--- src/main/java/com/snakeandladder/App.java
+++ src/main/java/com/snakeandladder/App.java
@@ -1,15 +1,21 @@
 package com.snakeandladder;
-
+import java.util.Random;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static Random random = new Random();
+	static int rollDie() {
+		int diceValue = (random.nextInt(10) % 6)+1;
+		return diceValue;
+	}
     public static void main( String[] args )
     {
         System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
         int startPosition = 0;
         int player1Position = startPosition;
+        System.out.println(rollDie());
     }
 }
