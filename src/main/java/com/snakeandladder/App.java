c1dbc6c5162681e5d0854213050e65bb93333fc2
suraj9741 <surajjadhav9741@gmail.com>
2020-11-19 00:59:52 +0530

    Add new second player and check condition of ladder

--- src/main/java/com/snakeandladder/App.java
+++ src/main/java/com/snakeandladder/App.java
@@ -14,44 +14,106 @@ public class App
 		int diceValue = (random.nextInt(10) % 6)+1;
 		return diceValue;
 	}
-	static int checkCondition(int player1Position, int rollDieValue) {
+	static int[] checkCondition(int player1Position, int rollDieValue , int playerTurn) {
+		int[] multipalValues = new int[2];
 		int condition = (random.nextInt(10) % 3);
 		switch(condition) {
 			case 0 :
 				player1Position = player1Position + 0;
+				if(playerTurn==0) {
+					playerTurn=1;
+				}
+				else {
+					playerTurn=0;
+				}
 				break;
 			case 1 :
 				player1Position = player1Position + rollDieValue;
+				if(playerTurn==0) {
+					playerTurn=0;
+				}
+				else {
+					playerTurn=1;
+				}
 				break;
 			case 2 :
 				player1Position = player1Position - rollDieValue;
+				if(playerTurn==0) {
+					playerTurn=1;
+				}
+				else {
+					playerTurn=0;
+				}
 				break;
 			default :
 				break;
 		}
-		return player1Position;
+		multipalValues[0]=playerTurn;
+		multipalValues[1]=player1Position;
+		return multipalValues;
 	}
-	static int checkDestination(int player1Position) {
-		int i=0;
-		while (player1Position<100) {
-			int rollDieValue = rollDie();
-			player1Position = checkCondition(player1Position , rollDieValue);
-			if(player1Position < 0) {
-				player1Position = setPosition();
-			}
-			else if(player1Position > 100) {
-				player1Position = player1Position - rollDieValue;
-			}
-			System.out.println(i+" = "+ player1Position);
-			i++;
+	static int[] checkDestination(int player1Position , int player) {
+		int rollDieValue = rollDie();
+		int[] multipalValues = new int[2];
+		multipalValues = checkCondition(player1Position, rollDieValue, player);
+		player1Position = multipalValues[1];
+		if(player1Position < 0) {
+			player1Position = setPosition();
 		}
-		System.out.println("Dies roll : "+ (i-1) + "\nPlayer final position : " + player1Position);
-		return player1Position;
+		else if(player1Position > 100) {
+			player1Position = player1Position - rollDieValue;
+		}
+		System.out.println("Player "+ (player+1) +" = "+ player1Position);
+		//i++;
+		//System.out.println("Dies roll : "+ (i-1) + "\nPlayer final position : " + player1Position);
+		player=multipalValues[0];		
+		multipalValues[0]=player;
+		multipalValues[1]=player1Position;
+		return multipalValues;
+	}
+	static int toss() {
+		int playerTurn = random.nextInt(10) % 2;
+		if (playerTurn == 0) {
+			System.out.println("Player 1 win the toss");
+		}
+		else {
+			System.out.println("Player 2 win the toss");
+		}
+		return playerTurn;
+	}
+	static void startGame() {
+		int player1Position = setPosition();
+		int player2Position = setPosition();
+		int position = setPosition();
+		int playerTurn = toss();
+		int[] multipalValues = new int[2];
+		while(position<100) {
+			if(playerTurn == 0) {
+				multipalValues = checkDestination(player1Position , playerTurn);
+				player1Position = multipalValues[1];
+				position = player1Position;
+				playerTurn = multipalValues[0];
+				if(position == 100) {
+					System.out.println("Player 1 win the Game");
+				}
+			}
+			else {
+				multipalValues = checkDestination(player2Position , playerTurn);
+				player2Position = multipalValues[1];
+				position = player2Position;
+				playerTurn = multipalValues[0];
+				if(position == 100) {
+					System.out.println("Player 2 win the Game");
+				}
+			}
+		}
+	}
+	static void playGame() {
+		
 	}
 	public static void main( String[] args )
 	{
-		System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
-		int player1Position = setPosition();
-		checkDestination(player1Position);
+		System.out.println( "--------------------- Welcome to Snake and ladder Game ---------------------" );
+		startGame();
 	}
 }
