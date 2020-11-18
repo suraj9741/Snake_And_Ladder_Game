fa250322dbb3f67627d02f2e79f69f860e638620
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 23:35:59 +0530

    Add function for check Player Reached at Destination or Not

--- src/main/java/com/snakeandladder/App.java
+++ src/main/java/com/snakeandladder/App.java
@@ -7,32 +7,50 @@ import java.util.Random;
 public class App 
 {
 	static Random random = new Random();
+	static int setPosition() {
+		return 0;
+	}
 	static int rollDie() {
 		int diceValue = (random.nextInt(10) % 6)+1;
 		return diceValue;
 	}
-	static int checkCondition(int player1Position) {
+	static int checkCondition(int player1Position, int rollDieValue) {
 		int condition = (random.nextInt(10) % 3);
 		switch(condition) {
 			case 0 :
 				player1Position = player1Position + 0;
 				break;
 			case 1 :
-				player1Position = player1Position + rollDie();
+				player1Position = player1Position + rollDieValue;
 				break;
 			case 2 :
-				player1Position = player1Position - rollDie();
+				player1Position = player1Position - rollDieValue;
 				break;
 			default :
 				break;
 		}
 		return player1Position;
 	}
+	static int checkDestination(int player1Position) {
+		int i=0;
+		while (player1Position<100) {
+			int rollDieValue = rollDie();
+			player1Position = checkCondition(player1Position , rollDieValue);
+			if(player1Position < 0) {
+				player1Position = setPosition();
+			}
+			else if(player1Position > 100) {
+				player1Position = player1Position - rollDieValue;
+			}
+			System.out.println(i+" = "+ player1Position);
+			i++;
+		}
+		return player1Position;
+	}
 	public static void main( String[] args )
 	{
 		System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
-		int startPosition = 0;
-		int player1Position = startPosition;
-		System.out.println(checkCondition(player1Position));
+		int player1Position = setPosition();
+		System.out.println(checkDestination(player1Position));
 	}
 }
