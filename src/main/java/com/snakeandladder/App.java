357f6787974bb6d12b686dd5a1b9e5a62adb6683
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 22:39:37 +0530

    Set Starting position of player 1

--- src/main/java/com/snakeandladder/App.java
+++ src/main/java/com/snakeandladder/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome in Snake and ladder Game" );
+        System.out.println( "---------------------Welcome in Snake and ladder Game---------------------" );
+        int startPosition = 0;
+        int player1Position = startPosition;
     }
 }
