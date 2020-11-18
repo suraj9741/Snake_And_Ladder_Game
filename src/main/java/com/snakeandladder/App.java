ffafa71d3f1e91fe1229b1708c43fa1e167ca008
suraj9741 <surajjadhav9741@gmail.com>
2020-11-18 22:27:50 +0530

    Welcome message for Snake and Ladder Game

---
+++ src/main/java/com/snakeandladder/App.java
@@ -0,0 +1,13 @@
+package com.snakeandladder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome in Snake and ladder Game" );
+    }
+}
